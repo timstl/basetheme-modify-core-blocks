ff21436a5f0f2b0f19b04b2f969be007107b9f36
Tim Gieseking <tgieseking@atomicdust.com>
2019-05-02 16:10:44 -0500

    add styles for H1-H6; This doesn't change the element, but it applies the is-style-h# classes, which extend Bootstraps .h# classes.

--- basetheme-modify-core-blocks.js
+++ basetheme-modify-core-blocks.js
@@ -24,3 +24,31 @@ wp.blocks.registerBlockStyle("core/separator", {
 	name: "short-centered",
 	label: "Short Centered"
 });*/
+
+/**
+ * Add heading styles.
+ */
+wp.blocks.registerBlockStyle("core/heading", {
+	name: "h1",
+	label: "H1"
+});
+wp.blocks.registerBlockStyle("core/heading", {
+	name: "h2",
+	label: "H2"
+});
+wp.blocks.registerBlockStyle("core/heading", {
+	name: "h3",
+	label: "H3"
+});
+wp.blocks.registerBlockStyle("core/heading", {
+	name: "h4",
+	label: "H4"
+});
+wp.blocks.registerBlockStyle("core/heading", {
+	name: "h5",
+	label: "H5"
+});
+wp.blocks.registerBlockStyle("core/heading", {
+	name: "h6",
+	label: "H6"
+});
